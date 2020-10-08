fb2b33d15a0c80e1e8418854d7a4e0b0f4cb92d2
loia5tqd001 <loia5tqd001@gmail.com>
2020-10-08 04:07:55 +0000

    Deploying to gh-pages from  @ 63d81ff3a0af07eb0f80c31d5509914de8c0cd77 🚀

---
+++ precache-manifest.54f0551c31d57ae09be08d9e0d789ed4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4495bf7b58387a620a9636f26551bef5",
+    "url": "/Phonee/index.html"
+  },
+  {
+    "revision": "f7282b0a25e520925920",
+    "url": "/Phonee/static/css/2.0025f261.chunk.css"
+  },
+  {
+    "revision": "f7282b0a25e520925920",
+    "url": "/Phonee/static/js/2.a2f992ce.chunk.js"
+  },
+  {
+    "revision": "a0d95f8e00c43b2a4f493f339fc45c32",
+    "url": "/Phonee/static/js/2.a2f992ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b090384ef76de14ef1ad",
+    "url": "/Phonee/static/js/main.250be3ba.chunk.js"
+  },
+  {
+    "revision": "b5ad72f4dd93c3f42bb3",
+    "url": "/Phonee/static/js/runtime-main.f89ca281.js"
+  }
+]);
